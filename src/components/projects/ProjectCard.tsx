c672faac3822adcde3e98df7cfcc89ab2cd02285
Rehan Khurshid <rehankhurshid1@gmail.com>
2025-12-31 14:51:40 +0530

    Feat: Limit ProjectCard links to 5 for compact view

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -102,7 +102,7 @@ export const ProjectCard = React.memo(function ProjectCard({ project, onDelete }
       </CardHeader>
 
       <CardContent>
-        <LinkList projectId={project.id} links={project.links} />
+        <LinkList projectId={project.id} links={project.links} limit={5} />
         <AddLinkDialog
           onAddLink={async (title, url) => {
             await executeAddLink(() => handleAddLink(title, url));
